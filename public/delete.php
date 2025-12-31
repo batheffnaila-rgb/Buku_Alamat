fd34880d5d0951b6455a2fdf10dfe7b787e9fb03
amel2005-git <putuamel2005@gmail.com>
2025-12-31 13:36:48 +0800

    Update delete.php

--- public/delete.php
+++ public/delete.php
@@ -1,1 +1,15 @@
+<?php
+include '../config/database.php';
 
+if (!isset($_GET['id'])) {
+    header("Location: index.php");
+    exit;
+}
+
+$id = $_GET['id'];
+
+mysqli_query($koneksi, "DELETE FROM contacts WHERE id = $id");
+
+header("Location: index.php");
+exit;
+?>
